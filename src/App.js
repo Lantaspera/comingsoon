1b8bf61e77f04c95233f1902959cd12c7071928e
adrid sadanand <adridsadanand@gmail.com>
2023-02-16 00:22:56 +0530

    temp page modified

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import LaunchTimer from './Timer';
+import { Container, Row, Col } from 'react-bootstrap';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello</h1>
-      
-      </header>
-    </div>
+    <Container fluid className="App">
+      <Row xs={1} className='hedder_min'>
+        <Col><h1 className='Head_ing'>LAUNCHING SOON...</h1></Col>
+        <Col><LaunchTimer /></Col>
+      </Row>
+    </Container>
+
   );
 }
 
+
+
 export default App;
+
+
+
+
